ed24ebf54c55975a84f9e427b48adfcb48f38643
Jason Chiu <jason.chiu@codeium.com>
2025-07-25 20:59:09 +0000

    get rid of unowned files warning

--- internal/app/app.go
+++ internal/app/app.go
@@ -141,9 +141,9 @@ func (a *App) Run() (*OutputData, error) {
 	codeOwners.SetAuthor(author)
 
 	// Warn about unowned files
-	for _, uFile := range codeOwners.UnownedFiles() {
-		a.printWarn("WARNING: Unowned File: %s\n", uFile)
-	}
+	// for _, uFile := range codeOwners.UnownedFiles() {
+	// 	a.printWarn("WARNING: Unowned File: %s\n", uFile)
+	// }
 
 	// Print file owners if verbose
 	if a.config.Verbose {
